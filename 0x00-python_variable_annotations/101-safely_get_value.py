89f359e95389d7c82f0de6cd76ade305ec05d1da
youssef <youssefelghamour@gmail.com>
2024-06-06 17:23:30 +0000

    add 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,11 +6,8 @@ from typing import Dict, Any, TypeVar, Union
 T = TypeVar('T')
 
 
-def safely_get_value(
-    dct: Dict[Any, Any],
-    key: Any,
-    default: T = None
-) -> Union[Any, T]:
+def safely_get_value(dct: Dict[Any, Any], key: Any,
+                     default: T = None) -> Union[Any, T]:
     """ function that gets a value from a dictionary"""
     if key in dct:
         return dct[key]
