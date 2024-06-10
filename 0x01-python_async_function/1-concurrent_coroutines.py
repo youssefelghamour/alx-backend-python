58d970690f09bdb50713a4a285e3db5fc07a9906
youssef <youssefelghamour@gmail.com>
2024-06-10 14:27:09 +0000

    correct 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """ task 1 """
-import random
 import asyncio
 from typing import List
 
 
-wait_random = __import__('0-basic_async_syntax.py').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
