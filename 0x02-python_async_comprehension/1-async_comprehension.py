fc84b20e782a14cd7c56b7196e60405613cafc2b
youssef <youssefelghamour@gmail.com>
2024-06-11 10:28:02 +0000

    correct 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,4 +8,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """ function that returns a list of 10 random numbers using an async
+        comprehensing from async_generator """
     return [number async for number in async_generator()]
