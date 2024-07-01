5c2a71a2db9a355c565b5f14563c9816a62c1355
youssefelghamour <youssefelghamour@gmail.com>
2024-07-01 14:34:27 +0000

    task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 import unittest
 from parameterized import parameterized
 from client import GithubOrgClient
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -24,6 +24,22 @@ class TestGithubOrgClient(unittest.TestCase):
 
         # assert get_json was called once with the expected URL
         mock.assert_called_once_with(f'https://api.github.com/orgs/{org_name}')
+
+    def test_public_repos_url(self):
+        """ tests _public_repos_url function """
+        # create an instance of GithubOrgClient
+        test_client = GithubOrgClient("google")
+
+        # mock the org property with PropertyMock
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mck_org:
+            mck_org.return_value = {
+                "repos_url": "https://api.github.com/orgs/google/repos"
+            }
+
+            res = test_client._public_repos_url
+            # assert _public_repos_url returns the expected value
+            self.assertEqual(res, "https://api.github.com/orgs/google/repos")
 
 
 if __name__ == "__main__":
