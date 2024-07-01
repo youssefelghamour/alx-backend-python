917975d5e0a6c178fb0145054e5efa60fd7371de
youssefelghamour <youssefelghamour@gmail.com>
2024-07-01 08:02:11 +0000

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,6 +17,16 @@ class TestAccessNestedMap(unittest.TestCase):
         """ tests access_nested_map with different inputs """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ tests KeyError with the above inputs """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(repr(e.exception), f"KeyError('{str(path[-1])}')")
+
 
 if __name__ == "__main__":
     unittest.main()
