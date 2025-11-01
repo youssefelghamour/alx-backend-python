2588d1a137cfb78e6f2745d78143e6e6446c9144
youssef <youssefelghamour@gmail.com>
2025-11-01 16:42:36 +0100

    2. Batch processing Large Data

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -41,5 +41,4 @@ def batch_processing(batch_size):
     batch = next(stream_users_in_batches(batch_size))
 
     result = [user for user in batch if user['age'] > 25]
-    for user in result:
-        print(user)
+    return result
--- python-generators-0x00/1-main.py
+++ python-generators-0x00/1-main.py
@@ -4,6 +4,6 @@ processing = __import__('1-batch_processing')
 
 ##### print processed users in a batch of 50
 try:
-    processing.batch_processing(50)
+    print(processing.batch_processing(50))
 except BrokenPipeError:
     sys.stderr.close()
