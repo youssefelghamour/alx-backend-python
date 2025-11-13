4a1e15e8fcef40e9fff5c7ff8e270fade231d524
youssef <youssefelghamour@gmail.com>
2025-11-13 15:38:44 +0100

    7. Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -95,6 +95,17 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once_with(
                 TEST_PAYLOAD[0][0]["repos_url"])
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Tests has_license static method of GithubOrgClient"""
+        self.assertEqual(
+            GithubOrgClient.has_license(repo, license_key),
+            expected
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
