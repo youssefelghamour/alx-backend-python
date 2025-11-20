35563996b3e8a0c779fee6bb9f88bd915b4daa64
youssef <youssefelghamour@gmail.com>
2025-11-20 17:55:58 +0100

    0. Implement Authentication

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,4 +1,5 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated, BasePermission
 
 
 class IsAuthenticatedUser(IsAuthenticated):
