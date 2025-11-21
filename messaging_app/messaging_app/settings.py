10e0455148c33c5bf03fbba7ed1ca14d01470beb
youssef <youssefelghamour@gmail.com>
2025-11-21 14:54:51 +0100

    2. Pagination and Filtering

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -138,6 +138,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',         # secondary
         'rest_framework.authentication.BasicAuthentication',
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
 }
 
 
