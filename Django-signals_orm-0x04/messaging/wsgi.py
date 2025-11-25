e43d1504aee96bb3c2e340b24cbab86682b6769e
youssef <youssefelghamour@gmail.com>
2025-11-25 20:02:01 +0100

    0x04 Project Setup

--- Django-signals_orm-0x04/messaging/wsgi.py
+++ Django-signals_orm-0x04/messaging/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for messaging_app project.
+WSGI config for messaging project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django-signals_orm-0x04.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messaging.settings')
 
 application = get_wsgi_application()
